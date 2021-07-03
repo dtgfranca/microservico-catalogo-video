9439ce83ec366ce8c585bc84bca23423a1f18bc5
Diego França <diego.lightbase@sebraemg.com.br>
2021-07-03 17:36:30 +0000

    feat: cria resource gategory e resource gender

---
+++ database/seeds/GenderSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\Gender;
+use Illuminate\Database\Seeder;
+
+class GenderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        factory(Gender::class, 2)->create();
+    }
+}
